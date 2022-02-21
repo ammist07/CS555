69d06599d41e36a4bea37c764540a73bf0fc1a86
Vikramaditya <vikramaditya@vikramaityasmbp.myfiosgateway.com>
2022-02-21 15:48:37 -0500

    added private route logic

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import Home from './components/Home'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Signup from './components/Signup'
+import Protected from './components/Protected'
 
 const App = () => {
     return (
         <div className="App">
             <Routes>
+                <Route element={<Protected/>}>
+                    <Route exact path="/signup" element={<Signup />} />
+                </Route>
                 <Route exact path="/" element={<Home />} />
-                <Route exact path="/signup" element={<Signup />} />
             </Routes>
         </div>
     )
