e8c9d6ff352b97f18e449a6bef933df47936e4bc
Nareshhh <naresh.korapuri@gmail.com>
2022-02-22 01:27:59 +0530

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Login from './components/Login'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
 
 const App = () => {
     return (
-        <div>
-            App
+        <div className="App">
+            <Routes>
+                <Route exact path="/" element={<Login />} />
+            </Routes>
         </div>
     )
 }
-
-
 
 export default App
