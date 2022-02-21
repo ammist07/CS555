9aec98250d165bc08950ca73bda24ec7aabaf383
Anshul Mistry <amm.suthar@gmail.com>
2022-02-21 15:18:05 -0500

    added context

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider as UserProvider } from './context/UserContext'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      </BrowserRouter>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <UserProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </UserProvider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
