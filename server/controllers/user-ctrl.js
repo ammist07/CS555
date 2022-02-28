4ab7e2e2bdd7eb0deea3c8f02594c9f9b02fe576
NARESHHH <naresh.korapuri@gmail.com>
2022-02-28 17:42:03 -0500

    Function for creating a user when signup

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -18,6 +18,47 @@ const checkUser = async (req, res) => {
     }
     return res.json({success: false, message: 'Username or password incorrect'})
 }
+const createUser = async(req, res) => {
+    const body = req.body
+    body.password = await bcrypt.hash(body.password, 8);
+    console.log(body)
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a username, pass',
+        })
+    }
+    const users = await User.find({})
+    
+    for(let i = 0; i < users.length; i++) {
+        if (users[i].name === body.name) {
+            return res.json({success: false, message: 'User already exist'})
+        }
+    }
+
+    const user = await new User(body)
+
+    if (!user) {
+        return res.json({ success: false, error: err })
+    }
+    user
+        .save()
+        .then(() => {
+            return res.status(200).json({
+                success: true,
+                id: user._id,
+                name: user.name,
+            })
+        })
+        .catch(error => {
+            console.log('here')
+            return res.json({
+                success: false,
+                message: 'User not created!',
+            })
+        })
+}
 module.exports = {
-    checkUser
+    checkUser,
+    createUser
 }
--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 
 router.post('/login', UserCtrl.checkUser)
+router.post('/user', UserCtrl.createUser)
 
 module.exports = router
