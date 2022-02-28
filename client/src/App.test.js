d142da61a2a1bc6a58c1ccb0945af3786bd441cd
Anshul Mistry <amm.suthar@gmail.com>
2022-02-28 16:21:11 -0500

    test case fixed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './App'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Home from './components/Home'
+import { BrowserRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('App Page', () => {
+  render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  )
+})
--- client/src/context/createDataContext.js
+++ client/src/context/createDataContext.js
@@ -4,17 +4,17 @@ export default (reducer, actions, initialState) => {
 
     const Context = React.createContext()
 
-    const Provider = ({ children }) =>  {
+    const Provider = ({ children }) => {
         const [state, dispatch] = useReducer(reducer, initialState)
 
         const boundActions = {}
         Object.keys(actions).forEach((key) => {
             boundActions[key] = actions[key](dispatch)
         })
-        return (
-            <Context.Provider value={{ state, ...boundActions }}>
-                {children}
-            </Context.Provider>
+        return ( <
+            Context.Provider value = {
+                { state, ...boundActions } } > { children } <
+            /Context.Provider>
         )
 
     }
