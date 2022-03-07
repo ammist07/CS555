74ef45f8099e2baf6ded748e11982a14cb1b53e7
Nikileshwar043 <VangaNikhil559@gmail.com>
2022-03-08 03:20:25 +0530

    adding audio to user clicks

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./Cell.css"
 import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
-
+import sound from "./rename.mp3";
 
 const Cell = ({isLit, flipCellsAroundMe}) => {
+    let classes = "Cell" + (isLit ? " Cell-lit" : "")
+    let audio = new Audio(sound)
+
+    const handleClick = (evt) => {
+        audio.play()
+        flipCellsAroundMe()
+    }
 
     return (
         <td>
