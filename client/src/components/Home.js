ee7dd28c7d69d050faa6d51ed622793bdb294809
Vikramaditya <vikramaditya@Vikramadityas-MacBook-Pro.local>
2022-03-22 23:13:50 -0400

    create game logic of, bringing flowers in neighboring cells

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import Button from '@mui/material/Button'
 import { Link } from 'react-router-dom'
+import { Context as UserContext } from '../context/UserContext'
 
 
 const Home = () => {
+    const userContext = useContext(UserContext)
     return (
         <div>
-            <Button variant="contained" component={Link} to="/board">Login to Play!</Button>
-        </div>
+			{userContext.state.user ? (
+				<Button
+					data-testid='homebutton'
+					variant='contained'
+					id='paybutton'
+					component={Link}
+					to='/board'
+				>
+					Play!
+				</Button>
+			) : (
+				<Button
+					data-testid='homebutton'
+					variant='contained'
+					id='paybutton'
+					component={Link}
+					to='/board'
+				>
+					Login to Play!
+				</Button>
+			)}
+
+			<Button
+				data-testid='profilebutton'
+				variant='contained'
+				id='paybutton2'
+				component={Link}
+				to='/profile'
+			>
+				Profile
+			</Button>
+		</div>
     )
 }
 
