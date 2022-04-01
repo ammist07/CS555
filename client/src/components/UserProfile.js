6e3608ef96311607cfd982537fe63d32e1b435f5
NARESHHH <naresh.korapuri@gmail.com>
2022-03-31 22:13:45 -0400

    styling profile page

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,8 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+
 import { Context as UserContext } from '../context/UserContext'
 import { Stack, Typography, Paper } from '@mui/material'
 
 const UserProfile = () => {
+	const [done, setDone] = useState(false)
+	const [lb, setLB] = useState([])
 	const userContext = useContext(UserContext)
 	return (
 		<Paper
@@ -13,6 +16,14 @@ const UserProfile = () => {
 						color: 'white',
 					}}
 				>
+					 <Stack spacing={5} direction='row'>
+						<Typography variant='h5'>
+							Name: {userContext.state.user.name}
+						</Typography>
+						<Typography variant='h5'>
+							Games Played: {userContext.state.user.games}
+						</Typography>
+					</Stack>
 
 				</Paper>
 
