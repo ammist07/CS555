b468005a0a57748bb686da62bab8b979879ad2ec
NikhilaBenjaram <99777576+NikhilaBenjaram@users.noreply.github.com>
2022-03-31 20:00:26 -0400

    adding a drop down to select baord size

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,46 +2,81 @@ import React, {useContext, useState} from 'react'
 import Button from '@mui/material/Button'
 import { Link } from 'react-router-dom'
 import { Context as UserContext } from '../context/UserContext'
-
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
 
 const Home = () => {
     const userContext = useContext(UserContext)
 	const [diff, setDiff] = useState(3)
 	const [done, setDone] = useState(true)
+	const main = (
+		<React.Fragment>
+			<div>
+				{userContext.state.user ? (
+					<Button
+						data-testid='homebutton'
+						variant='contained'
+						id='paybutton'
+						component={Link}
+						to='/board'
+					>
+						Play!
+					</Button>
+				) : (
+					<Button
+						data-testid='homebutton'
+						variant='contained'
+						id='paybutton'
+						component={Link}
+						to='/board'
+					>
+						Login to Play!
+					</Button>
+				)}
+	
+				<Button
+					data-testid='profilebutton'
+					variant='contained'
+					id='paybutton2'
+					component={Link}
+					to='/profile'
+				>
+					Profile
+				</Button>
+				<br />
+				<br />
+				<br />
+				{userContext.state.isAuthenticated ? (
+					<RadioGroup
+						aria-labelledby='demo-radio-buttons-group-label'
+						defaultValue={3}
+						name='radio-buttons-group'
+						value={diff}
+						onChange={handleChange}
+					>
+						<FormControlLabel
+							value={3}
+							control={<Radio />}
+							label='Easy'
+						/>
+						<FormControlLabel
+							value={4}
+							control={<Radio />}
+							label='Medium'
+						/>
+						<FormControlLabel
+							value={5}
+							control={<Radio />}
+							label='Hard'
+						/>
+					</RadioGroup>
+				) : null}
+			</div>
+		</React.Fragment>
+	)	
     return (
-        <div>
-			{userContext.state.user ? (
-				<Button
-					data-testid='homebutton'
-					variant='contained'
-					id='paybutton'
-					component={Link}
-					to='/board'
-				>
-					Play!
-				</Button>
-			) : (
-				<Button
-					data-testid='homebutton'
-					variant='contained'
-					id='paybutton'
-					component={Link}
-					to='/board'
-				>
-					Login to Play!
-				</Button>
-			)}
-
-			<Button
-				data-testid='profilebutton'
-				variant='contained'
-				id='paybutton2'
-				component={Link}
-				to='/profile'
-			>
-				Profile
-			</Button>
-		</div>
+         <div>{done ? main : <div>Loading..</div>}</div>
     )
 }
 
