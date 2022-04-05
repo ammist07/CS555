7acd6d25bbff6f2025794b185de37061072b1069
NikhilaBenjaram <99777576+NikhilaBenjaram@users.noreply.github.com>
2022-04-04 22:14:33 -0400

    adding the useeffect in the import

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import Button from '@mui/material/Button'
 import { Link } from 'react-router-dom'
 import { Context as UserContext } from '../context/UserContext'
